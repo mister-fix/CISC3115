a2ea1f81122ce29be040793d3f298305a7136b19
Stephen <hellostephenwm@gmail.com>
2024-07-14 15:02:10 -0400

    removed comments

--- assignment-1/exercise-1/InfoOf3.java
+++ assignment-1/exercise-1/InfoOf3.java
@@ -2,26 +2,21 @@ import java.util.Scanner;
 
 public class InfoOf3 {
     public static void main(String[] args) {
-        // Opening a new keyboard input scanner
         Scanner input = new Scanner(System.in);
 
         int a = 0;
         int b = 0;
         int c = 0;
 
-        // Assignign value of a
         System.out.println("first number? ");
         a = input.nextInt();
 
-        // Assignign value of b
         System.out.println("second number? ");
         b = input.nextInt();
 
-        // Assignign value of c
         System.out.println("third number? ");
         c = input.nextInt();
 
-        // Calling all and printing all method results
         System.out.println("allAreEqual: " + allAreEqual(a, b, c));
         System.out.println("twoAreEqual: " + twoAreEqual(a, b, c));
         System.out.println("noneAreEqual: " + noneAreEqual(a, b, c));
@@ -30,7 +25,6 @@ public class InfoOf3 {
         System.out.println("strictlyAscending: " + strictlyAscending(a, b, c));
         System.out.println("strictlyDescending: " + strictlyDescending(a, b, c));
 
-        // closing keyboard input scanner
         input.close();
     }
 
