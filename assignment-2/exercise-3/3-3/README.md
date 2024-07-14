5fb935ca73d92fb50eae535f6e11581b24471670
Stephen <hellostephenwm@gmail.com>
2024-07-14 18:52:33 -0400

    declaring and initializing 2d array

---
+++ assignment-2/exercise-3/3-3/README.md
@@ -0,0 +1,3 @@
+# Instructions
+
+Declare and instantiate a 3x3 two-dimensional array of integers named tictactoe.
---
+++ assignment-2/exercise-3/3-3/Tictactoe.java
@@ -0,0 +1,5 @@
+class Tictactoe {
+    public static void main(String[] args) {
+        int[][] tictactoe = new int[3][3];
+    }
+}
